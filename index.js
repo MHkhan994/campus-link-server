831ea967694af999d05509442f9db5edcb41ff02
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-07-23 00:59:41 +0600

    single college, search college

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
 require('dotenv').config();
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmhrwse.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,12 +28,34 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const collegesCollection = client.db('campusLink').collection('colleges')
+
+        app.get('/colleges', async (req, res) => {
+            const result = await collegesCollection.find().toArray()
+            res.send(result)
+        })
+
+        // single college
+        app.get('/college/:id', async (req, res) => {
+            const id = req.params
+            const result = await collegesCollection.findOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+
+        app.get('/college/search/:text', async (req, res) => {
+            const text = req.params
+            const regex = new RegExp(text.text, "i")
+            const result = await collegesCollection.find({ name: regex }).toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
     }
 }
 run().catch(console.dir);
